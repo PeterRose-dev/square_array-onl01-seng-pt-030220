89e8bae592aa9be605eec828ab3848ff97969013
Peter Rose <peterroseq1@gmail.com>
2020-03-10 23:51:40 +0000

    Automatically backed up by Learn

--- square_array.rb
+++ square_array.rb
@@ -1,6 +1,20 @@
 def square_array(numbers)
-  numbers.each do |x| perfect_sq =  x*x 
-  numbers << perfect_sq
+  perfect_sqs = []
+  numbers.each do |x| perfect_sq =  x*x
+  perfect_sqs << perfect_sq
 end
-numbers
+perfect_sqs
 end
+
+numbers = [1,2,3]
+square_array(numbers)
+
+def square_array(new_numbers)
+  perfect_sqs = []
+  new_numbers.each do |x| perfect_sq =  x*x
+  perfect_sqs << perfect_sq
+end
+perfect_sqs
+end
+new_numbers = [9,10,16,25]
+square_array(new_numbers)
